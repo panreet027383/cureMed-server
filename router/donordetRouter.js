ecacc6984918bf6acb0253e4fa30dba1ffb81c89
Parneet Kaur <you@example.com>
2025-08-01 14:40:42 +0530

    chg

--- router/donordetRouter.js
+++ router/donordetRouter.js
@@ -2,9 +2,9 @@ var express=require("express");
 var appRouter=express.Router();
 
 var obj =require("../controller/donordetController");
-var {validateToken}=require("../config/validate");
+// var {validateToken}=require("../config/validate");
 
-appRouter.post("/donordetails/save", validateToken ,obj.doSave);
+appRouter.post("/donordetails/save" ,obj.doSave);
 
 appRouter.post("/donordetails/update",obj.doUpdate);
 
