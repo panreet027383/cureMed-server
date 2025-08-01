5b2702136d187ce29bd39706c1f6ea401e3939d4
Parneet Kaur <you@example.com>
2025-08-01 14:21:52 +0530

    chg

--- controller/userController.js
+++ controller/userController.js
@@ -6,10 +6,10 @@ const bcrypt = require("bcrypt");
 async function doSignup(req, resp) 
 {
     const { uid , pwd ,ut } = req.body;
-    const existingUser = await UserColRef.findOne({ uid });
-            if (existingUser) {
-            return resp.json({ status: false, msg: "User already exists" });
-        }
+    // const existingUser = await UserColRef.findOne({ uid });
+    //         if (existingUser) {
+    //         return resp.json({ status: false, msg: "User already exists" });
+    //     }
 
         const saltRounds = 10;
         const hashedPwd = await bcrypt.hash(pwd, saltRounds);
@@ -27,21 +27,39 @@ async function doSignup(req, resp)
         });
 }
 
-function doLogin(req, resp) {
-    UsercolRef.findOne({ uid: req.body.uid , pwd : req.body.pwd})
-        .then((doc) => {
-        if(doc)     
-        {
-            let jtoken = jwt.sign( { uid: req.body.uid, ut: doc.ut } , process.env.SEC_KEY, { expiresIn: "1h" });
-            resp.json( { status: true , msg: 'Logged in as ' + doc.ut , obj: doc,token: jtoken });
-        }
-        else
-            resp.json({ status: true, msg: "password wrong", obj: doc });
-        })
-        .catch((err) => {
-            resp.json({ status: false, msg: err.message });
-        });
+async function doLogin(req, resp) {
+  try {
+    const { uid, pwd } = req.body;
+
+    const doc = await UsercolRef.findOne({ uid });
+
+    if (!doc) {
+      return resp.json({ status: false, msg: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(pwd, doc.pwd); // compare plain with hashed
+    if (!isMatch) {
+      return resp.json({ status: false, msg: "Incorrect password" });
+    }
+
+    const jtoken = jwt.sign(
+      { uid: doc.uid, ut: doc.ut },
+      process.env.SEC_KEY,
+      { expiresIn: "1h" }
+    );
+
+    resp.json({
+      status: true,
+      msg: "Logged in as " + doc.ut,
+      obj: doc,
+      token: jtoken,
+    });
+  } catch (err) {
+    console.error("Login error:", err);
+    resp.status(500).json({ status: false, msg: "Server error during login" });
+  }
 }
+
 
 
 module.exports = { doSignup, doLogin }
